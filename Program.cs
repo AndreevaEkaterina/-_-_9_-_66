4e4a97bc2902323753f891ad1e7e662d26f2debe
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2022-11-13 15:07:07 +0800

    15:07

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 // натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30gi
-int Counter(int a, int b)
+int GetSum(int a, int b)
 {
     if (a == b)
     {
@@ -10,13 +10,13 @@ int Counter(int a, int b)
     }
     if (a > b)
     {
-        return b + Counter(a, b + 1);
+        return b + GetSum(a, b + 1);
     }
-    return a + Counter(a + 1, b);
+    return a + GetSum(a + 1, b);
 }
 Console.Clear();
 Console.Write("Введите первое число:");
 int n = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число:");
 int m = int.Parse(Console.ReadLine()!);
-Console.WriteLine(Counter(n, m));
+Console.WriteLine(GetSum(n, m));
